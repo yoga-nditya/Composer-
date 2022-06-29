7001593bb86757c8b26b06e1307bda9b1bf59e0c
Yoga Aditya Nugraha <108383702+yoga-nditya@users.noreply.github.com>
2022-06-29 11:31:51 +0700

    php composer init

---
+++ src/Customer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace BelajarPHP\Composer;
+
+class Customer 
+{
+    public function __construct(private string $name)
+    {
+
+    }
+
+    public function sayHello(string $name): string 
+    {
+        return "Hello $name, my name is $this->name";
+    }
+}
